1e2f61e6fc23c4a9b0f5e0b154988bc392bc29dc
emptist <jigme1968@gmail.com>
2021-04-17 11:32:12 +0800

    Initial Commit

---
+++ Shared/IOsDevTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOsDevTutorialApp.swift
+//  Shared
+//
+//  Created by jk on 2021/4/17.
+//
+
+import SwiftUI
+
+@main
+struct IOsDevTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
