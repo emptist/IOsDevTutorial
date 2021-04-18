f4ac27ef2f624501701897af665e409facd580d9
emptist <jigme1968@gmail.com>
2021-04-18 18:37:13 +0800

    add

---
+++ Shared/DateFormatText.swift
@@ -0,0 +1,28 @@
+//
+//  DateFormatText.swift
+//  IOsDevTutorial
+//
+//  Created by jk on 2021/4/17.
+//
+
+import SwiftUI
+
+struct DateFormatText: View {
+    let dt = Date()
+    static let dateFormatter: DateFormatter = {
+        var formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter
+    }()
+    var body: some View {
+        Text("It's \(dt, formatter: Self.dateFormatter)")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct DateFormatText_Previews: PreviewProvider {
+    static var previews: some View {
+        DateFormatText()
+    }
+}
