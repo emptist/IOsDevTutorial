529439511552727406740b8b09a368492c11b6ad
emptist <jigme1968@gmail.com>
2021-04-28 06:52:25 +0800

    add segmented Picker

--- Shared/ColorPicking.swift
+++ Shared/ColorPicking.swift
@@ -1,5 +1,5 @@
 //
-//  ColorPicker.swift
+//  ColorPicking.swift
 //  IOsDevTutorial
 //
 //  Created by jk on 2021/4/28.
@@ -7,23 +7,26 @@
 
 import SwiftUI
 
-struct ColorPicker: View {
+struct ColorPicking: View {
     @State var color = Color.black
     
     var body: some View {
         VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/) {
             Circle()
                 .frame(width: 200, height: 200)
-                .foregroundColor(.black)
+                .foregroundColor(color)
             Spacer()
                 .frame(height: 10)
-            //ColorPicker()
+            HStack {
+                ColorPicker("Select a color: ", selection:$color, supportsOpacity:true)
+            }
         }
+        .padding()
     }
 }
 
 struct ColorPicker_Previews: PreviewProvider {
     static var previews: some View {
-        ColorPicker()
+        ColorPicking()
     }
 }
---
+++ Shared/PickerIntoSegmentPicker.swift
@@ -0,0 +1,33 @@
+//
+//  PickerIntoSegmentPicker.swift
+//  IOsDevTutorial
+//
+//  Created by jk on 2021/4/28.
+//
+
+import SwiftUI
+
+struct PickerIntoSegmentPicker: View {
+    private var animals = ["🐱 cat", "🐹 dog", "🐨 pig"]
+    @State private var selectedAnimalIndex = 0
+    
+    var body: some View {
+        VStack {
+            Picker(selection: $selectedAnimalIndex, label: Text("A Picker ")) {
+                ForEach(0..<animals.count) {idx in
+                    Text(animals[idx]).tag(idx)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
+            Text("Your Choice: \(animals[selectedAnimalIndex])")
+        }
+        .padding()
+    }
+}
+
+struct PickerIntoSegmentPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerIntoSegmentPicker()
+    }
+}
